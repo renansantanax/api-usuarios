f3827feb8d8dc04f697ff5fe1db0892e33dc5427
renansantanax <renan.2r@live.com>
2026-05-11 08:50:37 -0300

    Configuração do CORS

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 
 const swaggerDocument = require("./docs/swagger");
@@ -12,6 +13,15 @@ app.use(express.json());
 
 //Rota para documentação Swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//Configuração CORS
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 //Registrando as rotas de usuário na aplicação
 app.use("/api", usuarioRoutes);
